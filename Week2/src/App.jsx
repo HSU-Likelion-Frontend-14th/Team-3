47d89fe4284b0e099d3d42499d9e7681463ff8d5
hyKim <rlagjsdud3@hansung.ac.kr>
2026-04-01 23:55:43 +0900

    feat: Hero 컴포넌트 구현

--- Week2/src/App.jsx
+++ Week2/src/App.jsx
@@ -1,8 +1,14 @@
 import './styles/App.scss'
 import Header from './components/Header'
+import Hero from './components/Hero'
 
 function App() {
-  return <Header />
+  return (
+    <>
+      <Header />
+      <Hero />
+    </>
+  )
 }
 
 export default App
